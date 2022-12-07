30207537548b12ccf1e6851ff0b0c6849ebbf920
Naitik Patel <naitik2002patel@gmail.com>
2022-12-07 22:35:50 +0530

    Create and read data completed

--- src/FormPage.js
+++ src/FormPage.js
@@ -7,8 +7,8 @@ import "react-toastify/dist/ReactToastify.css";
 import Table from "./Table";
 
 const FormPage = () => {
-//   const notify = () => toast("Success");
-  const [data,setData]=useState([]);
+  //   const notify = () => toast("Success");
+  const [data, setData] = useState([]);
   const [FriendsInfo, setFriendsInfo] = useState({
     name: "",
     age: 0,
@@ -27,6 +27,16 @@ const FormPage = () => {
         age: FriendsInfo.age,
         description: FriendsInfo.description,
       })
+      .than(() => {
+        setData([
+          ...data,
+          {
+            name: FriendsInfo.name,
+            age: FriendsInfo.age,
+            description: FriendsInfo.description,
+          },
+        ]);
+      });
     setFriendsInfo({ name: "", age: 0, description: "" });
   };
   useEffect(() => {
@@ -41,77 +51,98 @@ const FormPage = () => {
         setData(response.data);
       })
       .catch(() => {
-        console.log("err")
+        console.log("err");
       });
   }, []);
   return (
     <>
-    <div className="container-lg" style={{ minWidth: "50vw" }}>
-      <div className="input-group mb-3">
-        {/* <ToastContainer /> */}
-        <div className="input-group-prepend">
-          <button
-            className="btn btn-outline-secondary"
-            type="button"
-            id="button-addon1"
-          >
-            Friend's Name
-          </button>
+      <div className="container-lg" style={{ minWidth: "50vw" }}>
+        <div className="input-group mb-3">
+          {/* <ToastContainer /> */}
+          <div className="input-group-prepend">
+            <button
+              className="btn btn-outline-secondary"
+              type="button"
+              id="button-addon1"
+            >
+              Friend's Name
+            </button>
+          </div>
+          <input
+            name="name"
+            value={FriendsInfo.name}
+            type="text"
+            className="form-control"
+            placeholder
+            aria-label="Example text with button addon"
+            aria-describedby="button-addon1"
+            onChange={handlechange}
+          />
         </div>
-        <input
-          name="name"
-          value={FriendsInfo.name}
-          type="text"
-          className="form-control"
-          placeholder
-          aria-label="Example text with button addon"
-          aria-describedby="button-addon1"
-          onChange={handlechange}
-        />
-      </div>
 
-      <div className="form-group">
-        <label htmlFor="formGroupExampleInput" className="float-left">
-          Age
-        </label>
-        <input
-          value={FriendsInfo.age}
-          name="age"
-          type="number"
-          className="form-control"
-          id="formGroupExampleInput"
-          placeholder="Example input placeholder"
-          onChange={handlechange}
-        />
-      </div>
+        <div className="form-group">
+          <label htmlFor="formGroupExampleInput" className="float-left">
+            Age
+          </label>
+          <input
+            value={FriendsInfo.age}
+            name="age"
+            type="number"
+            className="form-control"
+            id="formGroupExampleInput"
+            placeholder="Example input placeholder"
+            onChange={handlechange}
+          />
+        </div>
 
-      <div className="form-group">
-        <label htmlFor="exampleFormControlTextarea1" className="float-left">
-          Add Description
-        </label>
-        <textarea
-          name="description"
-          value={FriendsInfo.description}
-          className="form-control"
-          id="exampleFormControlTextarea1"
-          rows={3}
-          onChange={handlechange}
-          defaultValue={""}
-        />
+        <div className="form-group">
+          <label htmlFor="exampleFormControlTextarea1" className="float-left">
+            Add Description
+          </label>
+          <textarea
+            name="description"
+            value={FriendsInfo.description}
+            className="form-control"
+            id="exampleFormControlTextarea1"
+            rows={3}
+            onChange={handlechange}
+            defaultValue={""}
+          />
+        </div>
+        <button
+          type="button"
+          onClick={handleSubmit}
+          class="btn btn-primary float-left mb-2"
+        >
+          Submit
+        </button>
+        <div></div>
       </div>
-      <button
-        type="button"
-        onClick={handleSubmit}
-        class="btn btn-primary float-left mb-2"
-      >
-        Submit
-      </button>
-      <div>
+      <div className="container" style={{ marginTop: "20px" }}>
+        {/* <Table data={data} /> */}
+        <div>
+          <table class="table table-bordered table-hover">
+            <thead>
+              <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Age</th>
+                <th scope="col">Description</th>
+              </tr>
+            </thead>
+            {data.map((i) => (
+              <tbody class="table-group-divider">
+                <tr>
+                  <td>{i.name}</td>
+                  <td>{i.age}</td>
+                  <td>{i.description}</td>
+                  <td><button className="btn btn-primary">Update</button></td>
+                  <td><button className="btn btn-danger">Delete</button></td>
+                </tr>
+              </tbody>
+            ))}
+          </table>
+        </div>
       </div>
-    </div>
-    <div className="container" style={{marginTop:"20px"}}>
-    <Table data={data}/>
-    </div>
     </>
   );
 };
--- src/Table.js
+++ src/Table.js
@@ -1,29 +1,28 @@
-import React from 'react'
+import React from "react";
 
-const Table = ({data}) => {
+const Table = ({ data }) => {
   return (
     <div>
-        <table class="table table-bordered table-hover">
-  <thead>
-    <tr>
-      <th scope="col">Name</th>
-      <th scope="col">Age</th>
-      <th scope="col">Description</th>
-    </tr>
-  </thead>
-  {data.map((i) => (
-    <tbody class="table-group-divider">
-    <tr>
-      <td>{i.name}</td>
-      <td>{i.age}</td>
-      <td>{i.description}</td>
-    </tr>
-  </tbody>
-   ))}
-  
-</table>
+      <table class="table table-bordered table-hover">
+        <thead>
+          <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Age</th>
+            <th scope="col">Description</th>
+          </tr>
+        </thead>
+        {data.map((i) => (
+          <tbody class="table-group-divider">
+            <tr>
+              <td>{i.name}</td>
+              <td>{i.age}</td>
+              <td>{i.description}</td>
+            </tr>
+          </tbody>
+        ))}
+      </table>
     </div>
-  )
-}
+  );
+};
 
-export default Table
+export default Table;
